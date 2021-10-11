850fae13d8d3b4e4ca93176c5725f52d3bb9e9fc
Babintcev <dmitry.babintsev1985@gmail.com>
2021-10-10 17:06:14 -0700

    Initial Commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        int[] arr = {4, 2, 3, 4, 5, 6, 7, 11, 20, 21};
+
+        System.out.println(a);
+        System.out.println(arr[0]);
+        System.out.println(arr[9]);
+
+    }
+}
